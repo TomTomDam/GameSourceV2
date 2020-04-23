882c3b521059ec2e8c2a09655825207a6fcb2deb
TomTomDam <thomasdam97@gmail.com>
2020-04-23 15:18:03 +0100

    Initial commit

---
+++ GameSource/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameSource.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
