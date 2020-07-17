455d84ef63c4c38550f3d548de872c7334262e72
TomTomDam <thomasdam97@gmail.com>
2020-07-17 15:56:36 +0100

    Set up tests for User and UserRole

---
+++ GameSource.Tests/Services/UserStatusServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSource.Tests.Services
+{
+    [TestFixture]
+    public class UserStatusServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
