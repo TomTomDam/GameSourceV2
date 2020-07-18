f6dcc816b0072ad98331d39f482808592c2ab56a
TomTomDam <thomasdam97@gmail.com>
2020-07-18 21:01:45 +0100

    Unit tests for all services for GetAll method - need to do other methods

--- GameSource.Tests/Services/GamesServiceTests.cs
+++ GameSource.Tests/Services/GamesServiceTests.cs
@@ -1,6 +1,16 @@
-﻿using NUnit.Framework;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using GameSource.Controllers.GameSource;
+using GameSource.Data.Repositories.GameSource.Contracts;
+using GameSource.Models.GameSource;
+using GameSource.Services.GameSource;
+using GameSource.Services.GameSource.Contracts;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GameSource.Tests.Services
@@ -8,10 +18,64 @@ namespace GameSource.Tests.Services
     [TestFixture]
     public class GamesServiceTests
     {
+        public GameService gameService;
+
+        public Mock<IGameRepository> mockGameRepo;
+        public Mock<IGameService> mockGameService;
+        public IFixture fixture;
+
         [SetUp]
         public void Setup()
         {
+            mockGameRepo = new Mock<IGameRepository>();
+            mockGameService = new Mock<IGameService>();
+            fixture = new Fixture();
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
+            gameService = new GameService(mockGameRepo.Object);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            mockGameRepo.Invocations.Clear();
+            mockGameService.Invocations.Clear();
+        }
+
+        [Test]
+        public void GetAll_ReturnsListOfGames()
+        {
+            var gamesList = fixture.Create<IEnumerable<Game>>();
+
+            mockGameRepo.Setup(x => x.GetAll()).Returns(gamesList);
+            mockGameService.Setup(x => x.GetAll()).Returns(gamesList);
+
+            var result = gameService.GetAll();
+
+            mockGameRepo.Verify(x => x.GetAll(), Times.Once());
+            //mockGameService.Verify(x => x.GetAll(), Times.Once());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<Game>>(result);
+            Assert.IsNotEmpty(result);
+        }
+
+        [Test]
+        public void GetAll_ReturnsEmptyList()
+        {
+            mockGameRepo.Setup(x => x.GetAll()).Returns(Enumerable.Empty<Game>());
+            mockGameService.Setup(x => x.GetAll()).Returns(Enumerable.Empty<Game>());
+
+            var result = gameService.GetAll();
+
+            mockGameRepo.Verify(x => x.GetAll(), Times.Once());
+            //mockGameService.Verify(x => x.GetAll(), Times.Once());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<Game>>(result);
+            Assert.IsEmpty(result);
         }
     }
 }
--- GameSource.Tests/Services/UserRoleServiceTests.cs
+++ GameSource.Tests/Services/UserRoleServiceTests.cs
@@ -1,6 +1,13 @@
-﻿using NUnit.Framework;
+﻿using AutoFixture;
+using GameSource.Data.Repositories.GameSourceUser.Contracts;
+using GameSource.Models.GameSourceUser;
+using GameSource.Services.GameSourceUser;
+using GameSource.Services.GameSourceUser.Contracts;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GameSource.Tests.Services
@@ -8,10 +15,64 @@ namespace GameSource.Tests.Services
     [TestFixture]
     public class UserRoleServiceTests
     {
+        public UserRoleService userRoleService;
+
+        public Mock<IUserRoleRepository> mockUserRoleRepo;
+        public Mock<IUserRoleService> mockUserRoleService;
+        public IFixture fixture;
+
         [SetUp]
         public void Setup()
         {
+            mockUserRoleRepo = new Mock<IUserRoleRepository>();
+            mockUserRoleService = new Mock<IUserRoleService>();
+            fixture = new Fixture();
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
+            userRoleService = new UserRoleService(mockUserRoleRepo.Object);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            mockUserRoleRepo.Invocations.Clear();
+            mockUserRoleService.Invocations.Clear();
+        }
+
+        [Test]
+        public void GetAll_ReturnsListOfUserRoles()
+        {
+            var userRolesList = fixture.Create<IEnumerable<UserRole>>();
+
+            mockUserRoleRepo.Setup(x => x.GetAll()).Returns(userRolesList);
+            mockUserRoleService.Setup(x => x.GetAll()).Returns(userRolesList);
+
+            var result = userRoleService.GetAll();
+
+            mockUserRoleRepo.Verify(x => x.GetAll(), Times.Once());
+            //mockUserRoleService.Verify(x => x.GetAll(), Times.Once());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<UserRole>>(result);
+            Assert.IsNotEmpty(result);
+        }
+
+        [Test]
+        public void GetAll_ReturnsEmptyList()
+        {
+            mockUserRoleRepo.Setup(x => x.GetAll()).Returns(Enumerable.Empty<UserRole>());
+            mockUserRoleService.Setup(x => x.GetAll()).Returns(Enumerable.Empty<UserRole>());
+
+            var result = userRoleService.GetAll();
+
+            mockUserRoleRepo.Verify(x => x.GetAll(), Times.Once());
+            //mockUserRoleService.Verify(x => x.GetAll(), Times.Once());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<UserRole>>(result);
+            Assert.IsEmpty(result);
         }
     }
 }
