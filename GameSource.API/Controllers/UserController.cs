3100256551418f4f42a73fe24e51ef68bdc186a4
TomTomDam <thomasdam97@gmail.com>
2020-07-19 13:55:48 +0100

    Setup vue js and api project

---
+++ GameSource.API/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameSource.Models.GameSourceUser;
+using GameSource.Services.GameSourceUser.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameSource.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await userService.GetAllAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetByID")]
+        public async Task<User> GetByID(int id)
+        {
+            return await userService.GetByIDAsync(id);
+        }
+
+        [HttpPost]
+        public async Task Insert([FromBody] User user)
+        {
+            await userService.InsertAsync(user);
+        }
+
+        [HttpPost]
+        public async Task Update([FromBody] User user)
+        {
+            await userService.UpdateAsync(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await userService.DeleteAsync(id);
+        }
+    }
+}
