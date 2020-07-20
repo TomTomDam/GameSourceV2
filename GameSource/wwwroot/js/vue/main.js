af93119b76317464e92de7dba851747eeabb7a59
TomTomDam <thomasdam97@gmail.com>
2020-07-20 16:13:44 +0100

    Messing with vue js script

--- GameSource/wwwroot/js/vue/main.js
+++ GameSource/wwwroot/js/vue/main.js
@@ -1,7 +1,13 @@
-﻿import Vue from 'vue'
-import App from './App.vue'
+﻿//import Vue from 'vue'
+//import App from './App.vue'
 
-new Vue({
-    render: h => h.(App),
-}).mount('#app')
+//new Vue({
+//    render: h => h.(App),
+//}).mount('#app')
 
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hi there!'
+    }
+})
