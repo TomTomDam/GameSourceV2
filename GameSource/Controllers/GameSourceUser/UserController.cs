65d8f4764d9a2113e014d997da7e8f3055f26643
TomTomDam <thomasdam97@gmail.com>
2020-07-27 14:15:05 +0100

    Created User controller for anything User related e.g. public profile, rather than combining it with Account things e.g. changing settings

---
+++ GameSource/Controllers/GameSourceUser/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameSource.Models.GameSourceUser;
+using GameSource.Services.GameSourceUser.Contracts;
+using GameSource.ViewModels.GameSourceUser.UserViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameSource.Controllers.GameSourceUser
+{
+    [Route("user")]
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet("profile/{id}")]
+        public async Task<IActionResult> Profile(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User user = await userService.GetByIDAsync((int)id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            UserProfileViewModel viewModel = new UserProfileViewModel
+            {
+
+            };
+
+            return View(viewModel);
+        }
+    }
+}
