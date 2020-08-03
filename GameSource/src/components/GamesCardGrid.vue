de5904659be834ecb64d83a9ddb4f81069d4f93e
TomTomDam <thomasdam97@gmail.com>
2020-08-03 14:49:15 +0100

    Messing around with vue. Need to configure webpack so it is using ./src/main.js instead of its own main.js

---
+++ GameSource/src/components/GamesCardGrid.vue
@@ -0,0 +1,30 @@
+﻿<template>
+    <div class="container">
+        <div class="row" v-if="games.length > 0">
+            <div class="col-md-3" v-for="game in games" v-bind:key="game">
+                <game-card :game="game"></game-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data: function () {
+            return {
+                games: []
+            };
+        },
+        methods: {
+            getAllGames() {
+                const { data } = axios.get('games/index');
+                this.games = data;
+            }
+        },
+        mounted() {
+            this.getAllGames();
+        }
+    }
+</script>
