1efd239b425e5e3414a1642a98ce71016163b456
TomTomDam <thomasdam97@gmail.com>
2020-08-13 12:04:07 +0100

    Set up repo and services for NewsArticle

---
+++ GameSource.Data/Repositories/GameSource/Contracts/INewsArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using GameSource.Models.GameSource;
+using System.Collections.Generic;
+
+namespace GameSource.Data.Repositories.GameSource.Contracts
+{
+    public interface INewsArticleRepository
+    {
+        public IEnumerable<NewsArticle> GetAll();
+        public NewsArticle GetByID(int id);
+        public void Insert(NewsArticle newsArticle);
+        public void Update(NewsArticle newsArticle);
+        public void Delete(int id);
+    }
+}
