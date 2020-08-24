144cbbf074591bd578cead0c3213938e8aee979c
TomTomDam <thomasdam97@gmail.com>
2020-08-24 16:51:05 +0100

    Working on UserProfileComment functionality. Upon loading UserProfile view, need to pass UserProfileID to UserProfileCommentViewModel

--- GameSource/Areas/GameSourceUser/ViewModels/UserProfileCommentViewModel/UserProfileCommentCreateViewModel.cs
+++ GameSource/Areas/GameSourceUser/ViewModels/UserProfileCommentViewModel/UserProfileCommentCreateViewModel.cs
@@ -4,6 +4,9 @@ namespace GameSource.Areas.GameSourceUser.ViewModels.UserProfileCommentViewModel
 {
     public class UserProfileCommentCreateViewModel
     {
+        public UserProfile UserProfile { get; set; }
+
+        public User Author { get; set; }
         public UserProfileComment UserProfileComment { get; set; }
     }
 }
