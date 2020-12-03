c18c1549204b6563636dcce0867d6aa9e4e83274
TomTomDam <thomasdam97@gmail.com>
2020-12-03 13:42:29 +0000

    Added NewsArticleCategory async methods, created NewsArticleCategory API controller to be called by vue components

---
+++ GameSource/API/Controllers/NewsArticleCategoryController.cs
@@ -0,0 +1,40 @@
+﻿using GameSource.Models;
+using GameSource.Models.Enums;
+using GameSource.Models.GameSource;
+using GameSource.Services.GameSource;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameSource.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsArticleCategoryController : ControllerBase
+    {
+        private readonly NewsArticleCategoryService newsArticleCategoryService;
+
+        public NewsArticleCategoryController(NewsArticleCategoryService newsArticleCategoryService)
+        {
+            this.newsArticleCategoryService = newsArticleCategoryService;
+        }
+
+        [HttpGet]
+        public IEnumerable<NewsArticleCategory> GetAll()
+        {
+            return newsArticleCategoryService.GetAll();
+        }
+
+        [HttpGet]
+        public async Task<ApiResponse> GetAllAsync()
+        {
+            var result = await newsArticleCategoryService.GetAllAsync();
+
+            if (result.Any())
+                return new ApiResponse(ResponseStatusCode.Success, "Successfully returned Users list.");
+
+            return new ApiResponse(ResponseStatusCode.Error, "Could not return Users list.");
+        }
+    }
+}
