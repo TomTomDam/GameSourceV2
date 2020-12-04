7e0f9991533d2e2e9d7b7273fe40f4cfaa4b0c4a
TomTomDam <thomasdam97@gmail.com>
2020-12-04 17:11:32 +0000

    Playing with News menu in Vue

--- GameSource/src/components/NewsCategories.vue
+++ GameSource/src/components/NewsCategories.vue
@@ -1,29 +1,30 @@
 ﻿<template>
     <div>
-        News Categories
+        <div>News Categories</div>
+        <li><a class="dropdown-item" v-for="category in categories" :key="category.id">{{ category.name }}</a></li>
     </div>
 </template>
 <script>
-    //export default {
-    //    data: function () {
-    //        return {
-    //            categories: []
-    //        };
-    //    },
-    //    methods: {
-    //        getAllCategories() {
-    //            this.$baseURL.get('api/news-article-category/GetAll')
-    //                .then(res => {
-    //                    this.categories = res.data;
-    //                    console.log("api/news-article-category/GetAll Success: " + res);
-    //                })
-    //                .catch(err => {
-    //                    console.log("api/news-article-category/GetAll Error: " + err);
-    //                });
-    //        }
-    //    },
-    //    created() {
-    //        this.getAllCategories();
-    //    }
-    //}
+    export default {
+        data: function () {
+            return {
+                categories: []
+            };
+        },
+        methods: {
+            getAllCategories() {
+                this.$baseURL.get('api/news-article-category/GetAll')
+                    .then(res => {
+                        this.categories = res.data;
+                        console.log("api/news-article-category/GetAll Success: " + res);
+                    })
+                    .catch(err => {
+                        console.log("api/news-article-category/GetAll Error: " + err);
+                    });
+            }
+        },
+        created() {
+            this.getAllCategories();
+        }
+    }
 </script>
