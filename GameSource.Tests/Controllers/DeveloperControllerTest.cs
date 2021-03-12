0064b1a1b8b16058a75d660d72480238cc50fe81
TomTomDam <thomasdam97@gmail.com>
2021-03-12 18:11:06 +0000

    Added unit tests. Need to figure out how to get AutoFixture to create instance of IFormFile

---
+++ GameSource.Tests/Controllers/DeveloperControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using GameSource.API.Controllers.GameSource;
+using GameSource.Infrastructure.Repositories.GameSource.Contracts;
+using GameSource.Models.GameSource;
+using GameSource.Infrastructure.Repositories.GameSource;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameSource.Models;
+
+namespace GameSource.Tests.Controllers
+{
+    [TestFixture]
+    public class DeveloperControllerTest
+    {
+        public DeveloperController developerController;
+        public DeveloperRepository developerRepository;
+        public Mock<IDeveloperRepository> mockDeveloperRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockDeveloperRepo = new Mock<IDeveloperRepository>();
+            developerController = new DeveloperController(mockDeveloperRepo.Object);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            mockDeveloperRepo.Invocations.Clear();
+        }
+
+        [Test]
+        public async Task GetAll_ReturnsListOfDevelopers()
+        {
+            var developerList = new List<Developer>()
+            {
+                new Developer { ID = 1, Name = "LucasArts"}
+            };
+
+            mockDeveloperRepo.Setup(x => x.GetAll()).Returns(developerList);
+
+            var result = await developerController.GetAll();
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<Task<ApiResponse>>(result);
+        }
+    }
+}
