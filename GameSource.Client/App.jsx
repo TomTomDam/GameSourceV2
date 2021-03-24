a9891c09ad2a88516f6e36d836f0ee8dc0a365f6
TomTomDam <thomasdam97@gmail.com>
2021-03-24 15:44:02 +0000

    Set up initial pages

--- GameSource.Client/App.jsx
+++ GameSource.Client/App.jsx
@@ -6,7 +6,17 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Login from "./components/Navbar/Login";
+import Login from "./components/Account/Login";
+import Register from "./components/Account/Register";
+import GameIndex from "./components/Games/Index";
+import DeveloperIndex from "./components/Developers/Index";
+import PublisherIndex from "./components/Publishers/Index";
+import PlatformIndex from "./components/Platforms/Index";
+import PlatformTypeIndex from "./components/PlatformTypes/Index";
+import ReviewIndex from "./components/PlatformTypes/Index";
+import ReviewCommentIndex from "./components/PlatformTypes/Index";
+import NewsArticleIndex from "./components/PlatformTypes/Index";
+import NewsArticleCategoryIndex from "./components/PlatformTypes/Index";
 
 const App = () => {
   return (
@@ -16,6 +26,16 @@ const App = () => {
       <Route path="/about" component={About}/>
       <Route path="/contact" component={Contact}/>
       <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/games" component={GameIndex}/>
+      <Route path="/developers" components={DeveloperIndex}/>
+      <Route path="/publishers" components={PublisherIndex}/>
+      <Route path="/platforms" components={PlatformIndex}/>
+      <Route path="/platform-types" components={PlatformTypeIndex}/>
+      <Route path="/reviews" components={ReviewIndex}/>
+      <Route path="/review-comments" components={ReviewCommentIndex}/>
+      <Route path="/news-articles" components={NewsArticleIndex}/>
+      <Route path="/news-article-categories" components={NewsArticleCategoryIndex}/>
     </div>
   );
 };
